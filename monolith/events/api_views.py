45775dae865cb7f23a9040a4969b7a1e512231bc
Joon Hyuk Brandon Jang <joonbjang@gmail.com>
2023-04-05 14:54:31 -0700

    Up to Creating a Location

--- monolith/events/api_views.py
+++ monolith/events/api_views.py
@@ -231,3 +231,22 @@ def api_show_location(request, pk):
             encoder=LocationDetailEncoder,
             safe=False,
         )
+
+
+@require_http_methods(["GET"])
+def api_list_states(request):
+    states = State.objects.order_by("name")
+
+    state_list = []
+
+    for state in states:
+        state_dict = {
+            'name': state.name,
+            'abbreviation': state.abbreviation,
+        }
+
+        state_list.append(state_dict)
+
+    return JsonResponse(
+        {'states': state_list},
+    )
